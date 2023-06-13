11c0f1760b24a3f7779b64f69840c109dec71fd7
broklyn321 <om321arka@gmail.com>
2023-06-13 20:53:59 +0530

    form and add btn added

---
+++ script.js
@@ -0,0 +1,16 @@
+let lib=[];
+
+function book()
+{
+
+}
+function addb()
+{
+
+}
+let newbb=document.querySelector("#addb");
+newbb.addEventListener("click", function() {
+    
+    let newf= document.querySelector("#info");
+    newf.style.display="block";
+})
