445407b34c24ea5dd45c4339e9f5181906c52638
broklyn321 <om321arka@gmail.com>
2023-06-14 21:10:33 +0530

    smth

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let lib=[];
 
-function book()
+function book(ti,au,pg,re)
 {
-
+    this.ti=ti;
+    this.au=au;
+    this.pg=pg;
+    this.re=re;
 }
 function addb()
 {
-
+    let ti=document.querySelector("#ti").value;
+    let au=document.getElementById("#au").value;
+    let pg=document.getElementById("#pg").value;
+    let re=document.getElementById("#re").checked;
+    let nbook= new book(ti,au,pg,re);
+    console.log(nbook);
 }
 let newbb=document.querySelector("#addb");
 newbb.addEventListener("click", function() {
     
     let newf= document.querySelector("#info");
     newf.style.display="block";
+})
+document.querySelector("#info").addEventListener("submit",function(){
+    event.preventDefault;
+    addb();
 })
